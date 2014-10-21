cfde4302f89548860da38bb2f251ef1f37a85c99
Mikolaj Olszewski <mikolajolszewski@MOB-NOT43.local>
2014-10-21 17:08:46 +0200

    Pierwsza wersja

--- Lesson 2/code/event_emitter/ee.js
+++ Lesson 2/code/event_emitter/ee.js
@@ -11,11 +11,30 @@
 	};
 
 	EE.prototype.on = function (eventName, listener, context) {
+        if (!this.listeners[eventName]) {
+            this.listeners[eventName] = [];
+        }
+        var table = this.listeners[eventName];
+        var binded = listener.bind(context);
+        table.push(binded);
 
+        return function () {
+            var index = table.indexOf(binded);
+            if (index > -1) {
+                table.splice(index, 1);
+            }
+        };
 	};
 
 	EE.prototype.emit = function (eventName /*, other args...*/) {
+        var args = [];
+        for (var i = 1; i < arguments.length; i++) {
+            args.push(arguments[i]);
+        }
 
+        for(var i in this.listeners[eventName]) {
+            this.listeners[eventName][i].apply(this, args);
+        }
 	};
 
 //	var ee = new EE();
