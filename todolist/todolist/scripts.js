1b74aa7910ff5bb8b04cfb0e80be5c7a5d9b076b
Mikolaj Olszewski <mikolajolszewski@MOB-NOT43.local>
2014-10-29 08:20:17 +0100

    Todo list

---
+++ todolist/todolist/scripts.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+  var list = document.querySelector("#list"), tab = [];
+
+  // Bind event of adding todo to the add button
+  document.querySelector(".add").addEventListener('click', function () {
+    var text = document.createElement('li'), input_value = document.querySelector("#newtodo").value;
+    console.log(input_value.replace(/\s/g, ''))
+    if (!input_value.replace(/\s/g, '') == "" && tab.indexOf(input_value) === -1) {
+      text.innerHTML = input_value + ' <span class="delete">Usuń</span>';
+      document.querySelector("#list").appendChild(text);
+      tab.push(input_value);
+      console.log(tab);
+      input_value = "";
+    } else {
+      alert("Task should not be empty and should not be the same as existing element!")
+    }
+  });
+
+  // Bind event of deleting todo
+  document.querySelector("#list").addEventListener('click', function (elem) {
+    var elem = event.target, parent = event.target.parentNode;
+    if (elem.tagName === 'SPAN') {
+       var deletedElementText = parent.textContent.replace(" Usuń", "");
+       //console.log(parent.textContent.replace(" Usuń", ""));
+       parent.parentNode.removeChild(parent);
+       tab.splice(tab.indexOf(deletedElementText), 1)
+       console.log(tab)
+    }
+  });
+};
+
